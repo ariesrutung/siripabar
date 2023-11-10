77a0cc08cfa7260e09be1ec07765633e3c4a7f73
acbarsyah <syahacbar@gmail.com>
2023-11-11 07:41:47 +0900

    grafik emonitoring

--- application/controllers/Emonitoring.php
+++ application/controllers/Emonitoring.php
@@ -7,12 +7,12 @@ class Emonitoring extends CI_Controller
     function __construct()
     {
         parent::__construct();
-        $this->load->model(['M_berita']);
+        $this->load->model(['M_emonitoring']);
     }
 
     public function index()
     {
-        $data['berita'] = $this->M_berita->get_all();
+        $data['emonitoring'] = $this->M_emonitoring->get_all();
         $data['title'] = 'EMONITORING PELAKSANAAN';
         $data['_view'] = "company/emonitor";
         $this->load->view('company/layout', $data);
