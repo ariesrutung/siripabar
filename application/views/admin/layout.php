cdd2dbd7d9f5732dc97e13050a67da9cdba3c73a
acbarsyah <syahacbar@gmail.com>
2023-11-11 01:31:16 +0900

    update favicon

--- application/views/admin/layout.php
+++ application/views/admin/layout.php
@@ -7,8 +7,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title><?php echo $title;?> | PANEL ADMIN SIRIPABAR </title>
-    <!-- Favicon icon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url(); ?>public/focus-theme/images/favicon.png">
+   
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo base_url(); ?>public/focus-theme/images/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo base_url(); ?>public/focus-theme/images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url(); ?>public/focus-theme/images/favicon/favicon-16x16.png">
+    <link rel="manifest" href="<?php echo base_url(); ?>public/focus-theme/images/favicon/site.webmanifest">
+    <link rel="mask-icon" href="<?php echo base_url(); ?>public/focus-theme/images/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+
+
     <link href="<?php echo base_url(); ?>public/focus-theme/vendor/pg-calendar/css/pignose.calendar.min.css" rel="stylesheet">
     <link href="<?php echo base_url(); ?>public/focus-theme/vendor/chartist/css/chartist.min.css" rel="stylesheet">
     <link href="<?php echo base_url(); ?>public/focus-theme/css/style.css" rel="stylesheet">
